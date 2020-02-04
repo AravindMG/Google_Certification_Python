066c95c680ee011c02c4cfc2e6a4b922c188ac37
AravindMG <aravind@bravenewcoin.com>
2020-02-05 09:55:25 +1300

    Initial commit

---
+++ Crash_course/assesment_one.py
@@ -0,0 +1,8 @@
+def number_of_seconds_in_a_week():
+    seconds_per_day = 86400
+    number_of_days_in_a_week = 7
+    print("Total number of seconds in a week = " + str(number_of_days_in_a_week * seconds_per_day))
+
+
+if __name__ == "__main__":
+    number_of_seconds_in_a_week()
---
+++ Crash_course/first_Script.py
@@ -0,0 +1,8 @@
+def script():
+    friends = ['Kavya', 'KR', 'Arjun']
+    for friend in friends:
+        print('Hi ' + friend)
+
+
+if __name__ == "__main__":
+    script()
