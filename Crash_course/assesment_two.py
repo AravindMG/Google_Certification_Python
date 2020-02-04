0b4f49b45ae31aae5524e7e9503e23d963c4d3f4
AravindMG <aravind@bravenewcoin.com>
2020-02-05 11:27:22 +1300

    assesment two deleted

--- Crash_course/assesment_two.py
+++
@@ -1,9 +0,0 @@
-def size_partition():
-    disk_size = 16 * 1024 * 1024 * 1024
-    sector_size = 512
-    sector_amount =  disk_size * sector_size
-    print(sector_amount)
-
-
-if __name__ == "__main__":
-    size_partition()
